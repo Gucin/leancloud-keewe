9a4c31cd7a0de57388cf0c7f096ed6093af742d6
YunXiao <yunxiaolai@keewe.net>
2015-05-20 17:39:01 +0800

    edit nickname display

--- cloud/mchat.js
+++ cloud/mchat.js
@@ -21,7 +21,7 @@ function getPushMessage(params, user) {
   };
   var msg = JSON.parse(contentStr);
   var msgDesc = getMsgDesc(msg);
-  var name = user.get('nickname');
+  var name = user.get('nick_name');
   json.alert = name + ' : ' + msgDesc;
   return JSON.stringify(json);
 }
